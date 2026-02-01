4f38ec9347ac9e4bb72bef385e3bf00ee5f706f5
nikilauda10 <20243ds050@utez.edu.mx>
2026-02-01 12:12:18 -0600

    Termine el examen de practica

--- examenNicholas/app.js
+++ examenNicholas/app.js
@@ -4,31 +4,35 @@ const btnBuscar = document.getElementById("btnBuscar");
 
 
 const recibirPoke = async() =>{
-if(response === ""){
-            setContent.innerHTML=("<div class`alert alert-warning`>Ingrese algo</div>")
-        }
-
+  
     try{
         const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+inputU.value);
-        
+        if(inputU.value===""){
+            setContent.innerHTML=`<div class="alert alert-warning" role="alert">
+  Ingrese el id del pokemon !!!
+</div>`;
+          return;
+        };
+        limpiar();
         if(!response.ok){
-            console.log("Warning");
-            return
+            setContent.innerHTML= `<div class="alert alert-danger" role="alert">
+            Pokemon no encontrado
+</div> `;
+            return;
         };
         const datos = await response.json();
         console.log(datos);
 
-         datos.sprites.forEach(element => {
         const divU = document.createElement("div");
-        divU.classList.add("col-md-3");
+        divU.classList.add("col-lg-12");
         divU.innerHTML=`<div class="card mb-3" style="max-width: 540px;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="${element.font_default}" class="img-fluid rounded-start" alt="...">
+      <img src="${datos.sprites.front_default}" class="img-fluid rounded-start" alt="...">
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">${element.name}</h5>
+        <h5 class="card-title">${datos.name}</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
       </div>
@@ -37,15 +41,15 @@ if(response === ""){
 </div> `
 
 setContent.appendChild(divU);
-    });
+    
     } catch{
         console.log("ERROR")
     }
-
    
-    
 };
 
-
+const limpiar = () =>{
+  setContent.innerHTML="";
+}
 
 btnBuscar.addEventListener("click",recibirPoke);
